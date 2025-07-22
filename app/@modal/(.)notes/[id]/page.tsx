0ca49a6a98b54db599a1886ae4a3b031bc7519b7
imyroslav <imyroslav@ukr.net>
2025-07-22 15:04:23 +0700

    correction-modal-n-id-page

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query';
-  import { fetchNoteById } from '../../../../lib/api/api';
+import { fetchNoteById } from "../../../../lib/api/serverApi";
   import NotePreview from './NotePreview.client';
   
   type NotePreviewProps = {
