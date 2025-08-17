65b0d058b8e7426d1ef0157c050b69369e9d21da
imyroslav <imyroslav@ukr.net>
2025-08-17 22:26:36 +0200

    correction-middleware

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,24 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { cookies } from 'next/headers';
-import { checkSession } from './lib/api/serverApi';
-import { parse } from 'cookie';
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { checkSession } from "./lib/api/serverApi";
+import { parse } from "cookie";
 
 const privateRoutes = ['/profile', '/notes'];
-const authRoutes = ['/sign-in', '/sign-up'];
+const publicRoutes = ['/sign-in', '/sign-up'];
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+  const refreshToken = cookieStore.get("refreshToken")?.value;
 
-  let accessToken = cookieStore.get('accessToken')?.value;
-  const refreshToken = cookieStore.get('refreshToken')?.value;
-
-  const isAuthRoute = authRoutes.some((route) => pathname.startsWith(route));
+  const isPublicRoute = publicRoutes.some((route) => pathname.startsWith(route));
   const isPrivateRoute = privateRoutes.some((route) => pathname.startsWith(route));
   
 
-  if (!accessToken && refreshToken) {
-    try {
+  if (!accessToken) {
+    if (refreshToken) {
+
       const data = await checkSession();
       const setCookie = data.headers['set-cookie'];
 
@@ -34,38 +34,37 @@ export async function middleware(request: NextRequest) {
           if (parsed.accessToken) cookieStore.set('accessToken', parsed.accessToken, options);
           if (parsed.refreshToken) cookieStore.set('refreshToken', parsed.refreshToken, options);
         }
-        accessToken = cookieStore.get('accessToken')?.value;
+
+        if (isPublicRoute) {
+          return NextResponse.redirect(new URL('/', request.url), {
+            headers: {
+              Cookie: cookieStore.toString(),
+            },
+          });
+        }
+
+          if (isPrivateRoute) {
+            return NextResponse.next({
+              headers: {
+                Cookie: cookieStore.toString(),
+              },
+            });
+          }
+        }
       }
-    } catch (err) {
-      console.error('Session check failed:', err);
+
+      if (isPublicRoute) {
+        return NextResponse.next();
+      }
+
+      if (isPrivateRoute) {
+        return NextResponse.redirect(new URL('/sign-in', request.url));
+      }
+    
+
     }
   }
 
-let isValidSession = false;
-
-if (accessToken) {
-  try {
-    const response = await checkSession();
-    if (response.status === 200) {
-      isValidSession = true;
-    }
-  } catch  {
-    isValidSession = false;
-  }
-}
-if (isValidSession) {
-  if (isAuthRoute) {
-    return NextResponse.redirect(new URL('/', request.url));
-  }
-  return NextResponse.next();
-}
-
-  if (isPrivateRoute) {
-    return NextResponse.redirect(new URL('/sign-in', request.url));
-  }
-
-  return NextResponse.next();
-}
 
 export const config = {
   matcher: ['/profile/:path*', '/notes/:path*', '/sign-in', '/sign-up'],
